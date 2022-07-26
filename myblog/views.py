9fad57708dc61886f15c5f355924d4bef840dfb3
JirniyJirniy <nik.vardi127@gmail.com>
2022-07-26 18:02:53 +0300

    Add Forms and Do SignUP page

--- myblog/views.py
+++ myblog/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import View
 from django.core.paginator import Paginator
+from django.contrib.auth import login
+from django.http import HttpResponseRedirect
+
+from .forms import SignUpForm
 from .models import Post
 
 class MainView(View):
@@ -12,4 +16,29 @@ class MainView(View):
         page_obj = paginator.get_page(page_number)
         return render(request, 'myblog/home.html', context={
             'page_obj': page_obj
+        })
+
+class PostDetailView(View):
+    def get(self, request, slug, *args, **kwargs):
+        post = get_object_or_404(Post, url=slug)
+        return render(request, 'myblog/post_detail.html', context={
+            'post':post
+        })
+
+class SignUpView(View):
+    def get(self, request, *args, **kwargs):
+        form = SignUpForm()
+        return render(request, 'myblog/signup.html', context={
+            'form': form
+        })
+
+    def post(self, request, *args, **kwargs):
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            if user is not None:
+                login(request, user)
+                return HttpResponseRedirect('/')
+        return render(request, 'myblog/signup.html', context={
+            'form': form,
         })
