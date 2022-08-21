29d6ac1e5cb498088579f240d95e6b350729a382
Nikita <66023572+jirniyjirniy@users.noreply.github.com>
2022-08-21 16:55:17 +0300

    Update settings.py

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -167,4 +167,4 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'nik.vardi127@gmail.com'
-EMAIL_HOST_PASSWORD = 'qzxzpvqmrwyinqou'
+EMAIL_HOST_PASSWORD = ''
